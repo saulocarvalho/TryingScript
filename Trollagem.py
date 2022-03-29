0e31bf8f8dce1e22adde7bc9477cd5195136c843
saulocarvalho <saulodias60@gmail.com>
2022-03-29 20:12:22 -0300

    adaptação

--- Trollagem.py
+++ Trollagem.py
@@ -3,14 +3,13 @@ import time
 
 #pyautogui.click('windows2.png')
 #pyautogui.write("notepad")
-#pyautogui.keyDown('win')
-#pyautogui.keyUp('win')
-pyautogui.press('win')
-pyautogui.PAUSE=2
+pyautogui.PAUSE=1
+pyautogui.keyDown('win')
+pyautogui.press('r')
+pyautogui.keyUp('win')
 pyautogui.write("notepad")
 pyautogui.press('enter')
-#pyautogui.press('enter')
-pyautogui.write("Kct abriu sozinho")
+pyautogui.write("Minha nossa :0 abriu sozinho")
 pyautogui.write("\n voce esta sendo hackeado pelo #saulinhohacker")
 
 #pip install auto-py-to-exe // auto-py-to-exe
