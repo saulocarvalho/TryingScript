624c25d113c089cc93ea87d04a738b1d93fb29b4
SauloCarvalho <saulodias60@gmail.com>
2022-04-08 17:29:09 -0300

    try on jupyter

--- Trollagem.py
+++ Trollagem.py
@@ -13,3 +13,4 @@ pyautogui.write("Minha nossa :0 abriu sozinho")
 pyautogui.write("\n voce esta sendo hackeado pelo #saulinhohacker")
 
 #pip install auto-py-to-exe // auto-py-to-exe
+#interval=1 (1 segundo de pause entre cada tela, ex = n o t e p a d)
--- Window.py
+++ Window.py
@@ -1,6 +1,14 @@
 import tkinter
+import pandas_datareader 
 
-janela = tkinter.Tk()
-janela.title("Teste")
+#janela = tkinter.Tk()
+#janela.title("Teste")
 
-janela.mainloop()
+#janela.mainloop()
+
+from pandas_datareader import data as web
+import pandas as pd
+import matplotlib.pyplot as plt
+
+cotacao_bovespa = web.DataReader('^BVSP',data_source='yahoo', start="1/1/2020", end="1/1/2021")
+display(cotacao_bovespa)
